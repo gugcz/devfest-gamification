1705ac912ef792c18b68916c9bcf8d0499973c53
Jirka Korejtko <jirka@korejtko.cz>
2013-10-26 01:27:14 +0200

    Fixed list of achievements when login

--- web/achievements.php
+++ web/achievements.php
@@ -7,16 +7,27 @@
                     <th>&nbsp;</th>
                     <th>Název</th>
                     <th>Místo</th>
+                    <th>Stav</th>
                 </tr>
                 <?php
-                $result = dibi::query("SELECT * FROM achievements
-                 LEFT JOIN log ON achievement_id = id
-                 WHERE gplus_id = %s
+                $result = dibi::query("SELECT id, name, location, nice_image, basic_image, congrats_text, unlocked_time FROM achievements
+                 LEFT JOIN (SELECT achievement_id, unlocked_time FROM log WHERE gplus_id = %s) d ON achievement_id = id
                  ORDER BY id", $me['id']);
 
                 foreach ($result as $achievement) {
-                    $image = $achievement['nice_image'];
-                    echo "<tr><td><img src='" . $image . "' width='50'></td><td>" . $achievement['name'] . "</td><td>" . $achievement['location'] . "</td></tr>";
+                    if ($achievement['unlocked_time'] === null) {
+                        $image = $achievement['basic_image'];
+                        $unlocked = "Nesplněno";
+                    } else {
+                        $image = $achievement['nice_image'];
+                        $unlocked = "Splněno";
+                    }
+                    echo "<tr>
+                            <td><img src='" . $image . "' width='50'></td>
+                            <td>" . $achievement['name'] . "</td>
+                            <td>" . $achievement['location'] . "</td>
+                            <td>" . $unlocked . "</td>
+                          </tr>";
                 }
                 ?>
             </table>
