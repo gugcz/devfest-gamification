a72d2665044026061b0fa13afa1dbb6f0fdacfb8
Jirka Korejtko <jirka@korejtko.cz>
2013-10-27 02:21:04 +0200

    redirect to new domain (game.devfest.cz) ;-)

--- web/includes/header.php
+++ web/includes/header.php
@@ -4,7 +4,7 @@ require_once("config.php");
 if (isset($_GET['code'])) {
     $client->authenticate();
     $_SESSION['token'] = $client->getAccessToken();
-    $redirect = 'http://' . $_SERVER['HTTP_HOST'] . "/";
+    $redirect = $nastaveni['API_RedirectUri'];
     header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
 }
 
--- web/index.php
+++ web/index.php
@@ -5,10 +5,13 @@
     <tr><th>&nbsp;</th><th>Jméno soutěžícího</th><th>Počet bodů</th></tr>
 <?php
 $result = dibi::query("SELECT * FROM leaderboard ORDER BY achievements_unlocked DESC, unlocked_last ASC");
-
+if(count($result) == 0){
+    echo "<tr><td>&nbsp;</td><td>Zatím nikdo nesoutěžil</td><td>&nbsp;</td></tr>";
+}
+else {
 foreach ($result as $player) {
     echo "<tr><td><img src='".$player['user_image'] ."?sz=50' alt=''></td><td>" . $player['user_name']."</td><td>".$player['achievements_unlocked']."</td></tr>";
-}
+}}
 ?>
 </table>
     </div>
