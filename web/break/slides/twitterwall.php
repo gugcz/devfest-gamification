65675adf095a9cbfa198a9f7a9e3e02d9bd96c5d
Jirka Korejtko <jirka@korejtko.cz>
2014-05-30 18:37:23 +0200

    Created new simple twitter wall

---
+++ web/break/slides/twitterwall.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Conference Twitter Wall</title>
+    <link rel="stylesheet" type="text/css" href="http://mdevgame.inmite.eu/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="http://mdevgame.inmite.eu/css/bootstrap-theme.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="app/screen.css"> -->
+
+    <!-- make your customisations to the twitter wall using the custom.css -->
+    <!-- <link rel="stylesheet" type="text/css" href="custom.css"> -->
+    <link href="http://mdevgame.inmite.eu/css/theme.css" rel="stylesheet">
+    <style type="text/css">
+        body {
+            padding-top: 0;
+        }
+
+        .barvicka {
+            border-bottom: 4px #0081c5 solid;
+            padding: 10px;
+            height: 240px;
+            margin-bottom: 10px;
+        }
+
+        .row {
+            height: 748px;
+            overflow: hidden;
+        }
+
+        @media (min-width: 992px) {
+            .col-md-3 {
+                width: 242px;
+            }
+        }
+
+        @media (min-width: 1200px) {
+            .col-md-3 {
+                width: 292px;
+            }
+        }
+        @media (min-height: 769px) {
+		    .totalcenter {
+		        width: 520px;
+		        height: 768px;
+		        margin: -384px 0 0 -260px;
+		        zoom: 1.405;
+		    }
+		}
+        @media (max-height: 768px) {
+        	.totalcenter {
+        		width: 520px;
+        		margin: 0 auto;
+        	}
+        }
+    </style>
+</head>
+<body>
+<div class="totalcenter">
+<a class="twitter-timeline"
+data-dnt="true"
+href="https://twitter.com/search?q=%23mDevCamp"
+data-widget-id="472379928554373121"
+data-chrome="noheader nofooter noscrollbar transparent"
+height="760"
+
+>Tweety o "#mDevCamp"</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
+</body>
+</html> 
+ 
